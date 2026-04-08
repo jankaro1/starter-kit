05d96b740c14aee186cd7e8cd630bc0d342eda8a
Jan Karotki <jan.karotki@capgemini.com>
2026-04-08 12:52:00 +0000

    Master controller/view cleanup

--- stk/webapp/controller/CustomerDetails.controller.js
+++ stk/webapp/controller/CustomerDetails.controller.js
@@ -50,27 +50,3 @@ sap.ui.define([
 
     });
 });
-
-/*sap.ui.define([
-    "sap/ui/core/mvc/Controller",
-],
-
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller 
-     */
-/*function (Controller) {
-  "use strict";
-
-return Controller.extend("stk.stk.controller.CustomerDetails", {
-      onInit: function() {
-        var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-        oRouter.getRoute("CustomerDetails").attachPatternMatched(this._onPatternMatched, this);
-      },
-      _onPatternMatched: function (oEvent) {
-        this.getView().bindElement({
-          path: "/Customers('" + oEvent.getParameter("arguments").CustomerID + "')",
-          parameters: {expand: "Orders, Orders/Order_Details"}
-        });
-      }
-  });
-});*/
